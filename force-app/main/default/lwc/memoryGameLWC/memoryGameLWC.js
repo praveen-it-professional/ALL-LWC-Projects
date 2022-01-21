47f6c1a2e0fe58598c1d716103f4341b197f86c8
Praveen Pujar <praveen.sfdcdeveloper@gmail.com>
2022-01-21 23:40:28 +0530

    memoryGame

--- force-app/main/default/lwc/memoryGameLWC/memoryGameLWC.js
+++ force-app/main/default/lwc/memoryGameLWC/memoryGameLWC.js
@@ -1,18 +1,3 @@
 import { LightningElement } from 'lwc';
-import {loadStyle} from 'lightning/platformResourceLoader';
-import fontawesome from '@salesforce/resourceUrl/fontawesome';
-export default class MemoryGameLWC extends LightningElement {
 
-    isLibLoaded
-    renderedCallback(){
-        //we are loading file in renderedCallback because whenever HTML file completes render,
-        //then only I want to load CSS
-        //load style is promise call
-        loadStyle(this,fontawesome+'/fontawesome/css/font-awesome.min.css').then(function(){
-            console.log("loaded successfully!!!!");
-        }).catch(error=>{
-            console.log(error);
-        });
-    }
-
-}
+export default class MemoryGameLWC extends LightningElement {}
